7886ce8099c73be143304488b3e40e355c8d8225
Erik Medeiros <erikmedeiros4@gmail.com>
2021-12-16 03:15:10 -0300

    Add the capability to calculate simple expressions

--- script.js
+++ script.js
@@ -5,21 +5,43 @@ const operations = {
     divide: (a, b) => a / b,
 }
 
+// must be in order of evaluation
 const symbols = {
-    "add": "+",
-    "subtract": "-",
-    "multiply": "*",
-    "divide": "/",
+    "/": "divide",
+    "*": "multiply",
+    "+": "add",
+    "-": "subtract",
 }
 
 function operate(operation, a, b) {
-    return operations[operation](a, b)
+    return +operations[operation](a, b).toFixed(8)
+}
+
+function evaluate(string) {
+    let output = string
+    for (let symbol in symbols) {
+        const regex = new RegExp(`([\\d](\\.[\\d])*)+ [\\${symbol}] ([\\d](\\.[\\d])*)+`, "g")
+        const expressions = output.match(regex)
+
+        if (expressions !== null) {
+            expressions.forEach(expression => {
+                [a, operation, b] = expression.split(" ")
+                const result = operate(symbols[operation], Number(a), Number(b))
+                output = output.replace(expression, result)
+            })
+        }
+    }
+
+    return output
 }
 
 const display = document.querySelector("#display")
 
 const clearButton = document.querySelector("#clear")
 clearButton.addEventListener("click", () => display.value = "")
+
+const equalsButton = document.querySelector("#equals")
+equalsButton.addEventListener("click", () => display.value = evaluate(display.value))
 
 const valueButtons = document.querySelectorAll("button.value")
 valueButtons.forEach(button => {
@@ -28,5 +50,5 @@ valueButtons.forEach(button => {
 
 const operatorButtons = document.querySelectorAll("button.operator")
 operatorButtons.forEach(button => {
-    button.addEventListener("click", () => display.value = ` ${symbols[button.value]} `)
+    button.addEventListener("click", () => display.value += ` ${button.value} `)
 })
