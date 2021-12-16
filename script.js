61a5b085dfaacfeaa44c6a27afbff5a06a45a20b
Erik Medeiros <erikmedeiros4@gmail.com>
2021-12-16 01:08:44 -0300

    Add basic layout and respond to user input

--- script.js
+++ script.js
@@ -4,6 +4,29 @@ const operations = {
     multiply: (a, b) => a * b,
     divide: (a, b) => a / b,
 }
+
+const symbols = {
+    "add": "+",
+    "subtract": "-",
+    "multiply": "*",
+    "divide": "/",
+}
+
 function operate(operation, a, b) {
     return operations[operation](a, b)
 }
+
+const display = document.querySelector("#display")
+
+const clearButton = document.querySelector("#clear")
+clearButton.addEventListener("click", () => display.value = "")
+
+const valueButtons = document.querySelectorAll("button.value")
+valueButtons.forEach(button => {
+    button.addEventListener("click", () => display.value += button.value)
+})
+
+const operatorButtons = document.querySelectorAll("button.operator")
+operatorButtons.forEach(button => {
+    button.addEventListener("click", () => display.value = ` ${symbols[button.value]} `)
+})
