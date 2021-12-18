6527aa1f62a56dedabf1ff4fad2aad8f2060d621
Erik Medeiros <erikmedeiros4@gmail.com>
2021-12-18 10:04:11 -0300

    Filter user input and handle invalid operations

--- script.js
+++ script.js
@@ -1,71 +1,97 @@
 const operations = {
-    add: (a, b) => a + b,
-    subtract: (a, b) => a - b,
-    multiply: (a, b) => a * b,
-    divide: (a, b) => a / b,
+	add: (a, b) => a + b,
+	subtract: (a, b) => a - b,
+	multiply: (a, b) => a * b,
+	divide: (a, b) => a / b,
 }
 
 // must be in order of evaluation
 const symbols = {
-    "/": "divide",
-    "*": "multiply",
-    "+": "add",
-    "-": "subtract",
+	"/": "divide",
+	"*": "multiply",
+	"+": "add",
+	"-": "subtract",
 }
 
 function operate(operation, a, b) {
-    return +operations[operation](a, b).toFixed(8)
+	return +operations[operation](a, b).toFixed(8)
 }
 
 function evaluate(string) {
-    let output = string
-    for (let symbol in symbols) {
-        const re = new RegExp(`([\-]?[\\d](\\.[\\d])*)+ [\\${symbol}] ([\-]?[\\d](\\.[\\d])*)+`, "g")
-        const expressions = output.match(re)
+	let output = string
+	for (let symbol in symbols) {
+		const re = new RegExp(
+			`\\-?[\\d]+(\\.?[\\d]+)? \\${symbol} \\-?[\\d]+(\\.?[\\d]+)?`,
+			"g"
+		)
+		const expressions = output.match(re)
 
-        if (expressions !== null) {
-            expressions.forEach(expression => {
-                [a, operation, b] = expression.split(" ")
-                const result = operate(symbols[operation], Number(a), Number(b))
-                output = output.replace(expression, result)
-            })
-        }
-    }
+		if (expressions !== null) {
+			expressions.forEach(expression => {
+				;[a, operation, b] = expression.split(" ")
+				const result = operate(symbols[operation], Number(a), Number(b))
+				output = output.replace(expression, result)
+			})
+		}
+	}
 
-    return output
+	return output
 }
 
 const display = document.querySelector("#display")
 
-document.querySelector("#clear")
-    .addEventListener("click", () => display.value = "")
+document.querySelector("#clear").addEventListener("click", () => (display.value = ""))
 
-document.querySelector("#negate")
-    .addEventListener("click", () => {
-        const exp = display.value.split(" ").at(-1)
-        const negated = exp.includes("-") ? exp.substring(1) : `-${exp}`
+document.querySelector("#negate").addEventListener("click", () => {
+	const exp = display.value.split(" ").at(-1)
 
-        display.value = display.value.replace(exp, negated)
-    })
+	if (exp.match(/^\-?[\d]+(\.?[\d]+)?$/) != null) {
+		const toggle = exp.includes("-") ? exp.slice(1) : `-${exp}`
+		display.value = display.value.replace(new RegExp(`${exp}$`), toggle)
+	}
+})
 
-document.querySelector("#decimal")
-    .addEventListener("click", () => {
-        const exp = display.value.split(" ").at(-1)
+document.querySelector("#decimal").addEventListener("click", () => {
+	const exp = display.value.split(" ").at(-1)
 
-        if(!exp.includes(".")) {
-            display.value += "."
-        }
-    })
+	if (exp.match(/^\-?[\d]+\.?$/) != null) {
+		const toggle = exp.includes(".") ? exp.slice(0, -1) : `${exp}.`
+		display.value = display.value.replace(new RegExp(`${exp}$`), toggle)
+	}
+})
 
-document.querySelector("#equals")
-    .addEventListener("click", () => display.value = evaluate(display.value))
+document.querySelector("#equals").addEventListener("click", () => {
+	if (display.value === "Invalid Expression") {
+		display.value = ""
+		return
+	}
 
-document.querySelectorAll("button.value")
-    .forEach(button => {
-        button.addEventListener("click", () => display.value += button.value)
-    })
+	const result = evaluate(display.value)
+	const invalidExp = result.split(" ").length !== 1 || result === "Infinity"
 
-document.querySelectorAll("button.operator")
-    .forEach(button => {
-        button.addEventListener("click", () => display.value += ` ${button.value} `)
-    })
+	display.value = invalidExp ? "Invalid Expression" : result
+})
+
+document.querySelectorAll("button.value").forEach(button => {
+	button.addEventListener("click", () => {
+		if (display.value === "Invalid Expression") {
+			display.value = ""
+		}
+
+		display.value += button.value
+	})
+})
+
+document.querySelectorAll("button.operator").forEach(button => {
+	button.addEventListener("click", () => {
+		if (display.value === "Invalid Expression") {
+			display.value = ""
+			return
+		}
+
+		const exp = display.value.split(" ").at(-1)
+		if (exp.match(/^\-?[\d]+(\.?[\d]+)?$/) != null) {
+			display.value += ` ${button.value} `
+		}
+	})
+})
